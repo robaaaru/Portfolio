e38796840e98573b29ec0ef7328c3186dca73383
Robert <robertangelotulagan@gmail.com>
2025-09-22 23:52:48 +0800

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+import Footer from './components/Footer.jsx'
 
 export default function App() {
     return(
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+        <h1>hello</h1>
+    )
+}
