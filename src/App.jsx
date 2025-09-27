848a784cdf43586691c1ac92ed6dca08b67adb9c
Robert <robertangelotulagan@gmail.com>
2025-09-28 02:14:13 +0800

    Shared footer state to App using callback prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Footer from './components/Footer.jsx'
-
+import { useState } from 'react'
 export default function App() {
+    const [currentPage, setPage] = useState('Default Page')
+    console.log(currentPage)
+
     return(
-        <Footer />
+        <Footer setPage={setPage}/>
     )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,21 @@
 
 import { Square, PanelsTopLeft, BriefcaseBusiness, PencilLine, Link } from 'lucide-react';
 
-export default function Footer(){
+export default function Footer({setPage}){
     let iconSize = 28
     let strokeWidth = 1.8
 
     return(
-    <footer className=" flex justify-center items-center gap-8  fixed bottom-0 w-[100%] p-5 border-t-[0.15rem] ">
-       <Square strokeWidth={1.8} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-       <PanelsTopLeft strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-       <BriefcaseBusiness strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-       <PencilLine strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-       <Link strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/> 
+    <footer  className=" flex justify-center items-center gap-8  fixed bottom-0 w-[100%] p-5 border-t-[0.15rem] ">
+       <Square onClick={()=>setPage('Default Page')} strokeWidth={1.8} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
+
+       <PanelsTopLeft onClick={()=>setPage('Projects')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
+
+       <BriefcaseBusiness onClick={()=>setPage('Experience')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
+
+       <PencilLine onClick={()=>setPage('Blogs')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
+
+       <Link onClick={()=>setPage('Social Media')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/> 
     </footer>
     )
 }
