eb95afd0d39ecb0e01bc12d74c6886cb7b53300c
Robert <robertangelotulagan@gmail.com>
2025-09-28 00:58:13 +0800

    Styled the footer and added icons WIP

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,16 @@
-import { Button } from "@/components/retroui/Button";
+
+import { Square, PanelsTopLeft, BriefcaseBusiness, PencilLine, Link } from 'lucide-react';
+
 export default function Footer(){
+    let iconSize = 24
+
     return(
-     <Button>Click Me!</Button>
+    <footer className=" flex justify-center items-center gap-8  fixed bottom-0 bg-[#f2ebbf] w-[100%] p-5 border-t-[0.17rem] ">
+       <Square color="#048688" size={iconSize}/>
+       <PanelsTopLeft  size={iconSize}/>
+       <BriefcaseBusiness size={iconSize}/>
+       <PencilLine size={iconSize}/>
+       <Link size={iconSize}/> 
+    </footer>
     )
 }
