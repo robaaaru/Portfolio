8ea8ca64f6f5a4287e67ef6ccbf27cfdbad19810
Robert <robertangelotulagan@gmail.com>
2025-09-28 02:52:19 +0800

    Refactored footer to make it more readable. Mapped contents inside the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,27 @@ import { Square, PanelsTopLeft, BriefcaseBusiness, PencilLine, Link } from 'luci
 export default function Footer({setPage}){
     let iconSize = 28
     let strokeWidth = 1.8
+    const baseClasses = 'hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'
+
+    const setPageHelper = () =>setPage('Default Page')
+
+    let icons = [
+        {icon: Square, key:'Square'},
+        {icon: PanelsTopLeft, key:'PanelsTopLeft'},
+        {icon: BriefcaseBusiness, key:'BriefcaseBusiness'},
+        {icon: PencilLine, key:'PencilLine'},
+        {icon: Link, key:'Link'},
+    ]   
+
+    let iconsArr = icons.map(element =>{
+        const Icon = element.icon
+        return <Icon key={element.key} onClick={setPageHelper} strokeWidth={strokeWidth} size={iconSize} className={baseClasses}/>
+    })  
 
     return(
+    
     <footer  className=" flex justify-center items-center gap-8  fixed bottom-0 w-[100%] p-5 border-t-[0.15rem] ">
-       <Square onClick={()=>setPage('Default Page')} strokeWidth={1.8} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-
-       <PanelsTopLeft onClick={()=>setPage('Projects')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-
-       <BriefcaseBusiness onClick={()=>setPage('Experience')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-
-       <PencilLine onClick={()=>setPage('Blogs')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/>
-
-       <Link onClick={()=>setPage('Social Media')} strokeWidth={strokeWidth} size={iconSize} className='hover:stroke-[#048688] hover:-translate-y-1 transition-transform duration-175 ease-in'/> 
+        {iconsArr}
     </footer>
     )
 }
