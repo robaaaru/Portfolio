5bccda9014122106f78c6e21fea1affd0ad91387
Robert <robertangelotulagan@gmail.com>
2025-09-29 04:04:48 +0800

    fix: adjust Footer component padding and icon class order for better responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer({ setPage }) {
   const strokeWidth = 1.8;
 
   const baseIconClasses =
-    "stroke-gray-700 hover:stroke-[#048688] lg:w-[1.7rem] lg:h-[1.75rem] w-[1.3rem] h-[1.3rem] hover:-translate-y-1 transition-transform duration-175 ease-in cursor-pointer";
+    "stroke-gray-700 hover:stroke-[#048688]  w-[1.3rem] h-[1.3rem] md:w-[1.445rem md:h-[1.445rem] lg:w-[1.7rem] lg:h-[1.75rem] hover:-translate-y-1 transition-transform duration-175 ease-in cursor-pointer";
   const wrapperClasses = "h-10";
 
   const icons = [
@@ -16,7 +16,7 @@ export default function Footer({ setPage }) {
   ];
 
   return (
-    <footer className="fixed bottom-0 w-full p-5 h-17 overflow-hidden backdrop-blur-2xl">
+    <footer className="fixed bottom-0 w-full p-2 md:p-4 lg:p-5 h-13 md:h-15 lg:h-18 overflow-hidden backdrop-blur-2xl">
       <div className="flex justify-center items-center gap-6">
         {icons.map(({ icon: Icon, key }, index) => (
           <button
